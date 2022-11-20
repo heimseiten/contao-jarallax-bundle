092c4ca17f129c6618a6c9726f87c3d7ba16622b
Niels <info@heimseiten.de>
2022-11-20 16:52:37 +0100

    Add files via upload

---
+++ src/HeimseitenContaoJarallaxBundle.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Jarallax Bundle.
+ *
+ * (c) heimseiten.de - Webdesign aus Köln 2022 <info@heimseiten.de>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/heimseiten/contao-jarallax-bundle
+ */
+
+namespace Heimseiten\ContaoJarallaxBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HeimseitenContaoJarallaxBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+    }
+}
